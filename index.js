cc044436363626636269086cd9b6b21ccb259c26
ElliotPhipps <phipps.elliot@gmail.com>
2020-04-20 19:03:44 -0400

    finished tasks 1 and 2

--- index.js
+++ index.js
@@ -12,20 +12,35 @@ console.log(fifaData);
 (d) Away Team goals for 2014 world cup final
 (e) Winner of 2014 world cup final */
 
+const finalMatch = fifaData.filter(function(item){
+    return item['Year'] === 2014 && item['Stage'] === 'Final';
+});
+console.log(finalMatch[0]['Home Team Name']);
+
+console.log(finalMatch[0]['Away Team Name']);
+
+console.log(finalMatch[0]['Home Team Goals']);
+
+console.log(finalMatch[0]['Away Team Goals']);
+
+console.log(finalMatch[0]['Win conditions']);
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
+function getFinals(data) {
 
-    /* code here */
-
+    const years = data.filter(function(item) {
+        return item['Stage'] === 'Final';
+    })
+    return years;
 };
+console.log(getFinals(fifaData));
 
 /* Task 3: Impliment a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
+function getYears(getFinals, years) {
 
-    /* code here */
+    
 
 };
 
